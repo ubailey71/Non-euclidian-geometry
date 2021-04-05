5706689eeaa68f52462a75a5c810cdc6702b4ff4
ubailey71 <ubailey71@gmail.com>
2021-04-05 17:50:05 +0300

    basic scene and character

---
+++ Non-euclidian/Assets/camera2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera2Controller : MonoBehaviour
+{
+    public Transform cam1;
+    public Transform cam2;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam2.transform.rotation = cam1.transform.localRotation;
+
+    }
+}
